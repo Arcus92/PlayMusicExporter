50bdea5e6736a2e35c0d31fe3a16c210cd4f6827
David Schulte <mail@david-schulte.de>
2015-01-23 19:33:19 +0100

    Add a LICENSE.txt and README.md

--- framework/src/main/java/de/arcus/framework/superuser/SuperUserCommand.java
+++ framework/src/main/java/de/arcus/framework/superuser/SuperUserCommand.java
@@ -1,10 +1,26 @@
 /*
- * Copyright (c) 2015.
+ * Copyright (c) 2015 David Schulte
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 package de.arcus.framework.superuser;
-
-import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -12,6 +28,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import de.arcus.framework.logger.Logger;
 
 /**
  * This class executes superuser commands.
@@ -130,8 +148,10 @@ public class SuperUserCommand {
             BufferedReader bufferedErrorReader = new BufferedReader(new InputStreamReader(SuperUser.getProcess().getErrorStream()));
 
             // Sends the command
-            for (String command : mCommands)
+            for (String command : mCommands) {
+                Logger.getInstance().logInfo("SuperUser", "< " + command);
                 dataOutputStream.writeBytes(command + "\n");
+            }
             dataOutputStream.flush();
 
             // TODO: This class cannot execute commands without any output (standard and error). These commands will run until the timeout will kill them!
@@ -162,7 +182,7 @@ public class SuperUserCommand {
                 // End of data
                 if (tmpLine == null) break;
 
-                Log.i("SuperUser", "> " + tmpLine);
+                Logger.getInstance().logInfo("SuperUser", "> " + tmpLine);
                 tmpList.add(tmpLine);
             }
             // Convert list to array
@@ -176,7 +196,7 @@ public class SuperUserCommand {
 
                 // End of data
                 if (tmpLine == null) break;
-                Log.e("SuperUser", "> " + tmpLine);
+                Logger.getInstance().logError("SuperUser", "> " + tmpLine);
 
                 tmpList.add(tmpLine);
             }
